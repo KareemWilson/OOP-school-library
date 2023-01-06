c9261ff093bb9502db0a210b4d3e72c2390175f0
KareemWilson <kareemmagdywilson@gmail.com>
2023-01-06 23:34:57 +0200

    Adjust teacher class

--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,8 @@
 require './person'
 
 class Teacher < Person
-  def initialize(name, age, parent_permission, specialization)
-    super(age, name, parent_permission)
+  def initialize(name, age, specialization)
+    super(age, name)
     @specialization = specialization
   end
 
